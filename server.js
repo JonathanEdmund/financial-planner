98a2235db87062a43c25447705bde32de0ba1afd
JonathanEdmund <jonathan.edmund@gmail.com>
2022-09-04 17:37:20 +0700

    modified server port

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ async function main() {
 app.use("/api/items", items);
 app.use("/api/users", users);
 
-if (process.env.NODE_ENV === "production") {
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
